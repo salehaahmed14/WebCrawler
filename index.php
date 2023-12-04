6dd073827896d4abcaa4825b89fa0c0300326718
salehaahmed14 <113420201+salehaahmed14@users.noreply.github.com>
2023-12-04 20:12:53 +0500

    Add checks for Robots.txt

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 //function calls to populate the database
 include('database.php');
 include('crawler.php');
+include('isCrawlingAllowed.php');
 $seedUrl = "https://en.wikipedia.org/wiki/JavaScript";
-create_and_connect_to_database("localhost", "root", "");
-crawl_page($seedUrl, 2);
+if (isCrawlingAllowed($seedUrl)){
+    create_and_connect_to_database("localhost", "root", "");
+    crawl_page($seedUrl, 2);
+}
+else{
+    echo "Crawling disallowed.";
+}
 ?>
